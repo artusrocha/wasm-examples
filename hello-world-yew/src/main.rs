30c9396a03eee0f3b895792d0f30b6a28850c3f3
Artus Rocha <artusrocha@gmail.com>
2020-04-19 08:54:50 -0300

    add stylesheet

--- hello-world-yew/src/main.rs
+++ hello-world-yew/src/main.rs
@@ -45,17 +45,19 @@ impl Component for HelloWorld {
     }
 
     fn view(&self) -> Html {
-        let button_text = if self.clicked { "add another row" } else { "add row" };
+        let button_text = if self.counter > 0 { "add another row" } else { "add row" };
         html! {
             <div>
                 <div>{ "Total rows: " }{ self.counter }</div>
                 <div>
                     <button onclick=&self.plus_row>{ button_text }</button>
-                    {   if self.counter > 0 { html!{ <button onclick=&self.minus_row>{ "Remove row" }</button> } }
-                        else { html!{ <span/> } }
+                    {   if self.counter > 0 {
+                            html!{ <button onclick=&self.minus_row>{ "Remove row" }</button> }
+                        } else { html!{ <span/> } }
                     }
                 </div>
                 { rows(self.counter) }
+                { basic_style() }
             </div>
         }
     }
@@ -66,20 +68,28 @@ impl Component for HelloWorld {
 fn rows(counter: i32) -> Html {
     html! {
         <table>{
-            for (1..=counter).map(|i| html! {
-                <tr>
-                    <td>{format!{"Hello World! :: {}", i}}</td>
-                    <td>{ if i.is_even() {"even"} else {"odd"} }</td>
-
-
-                </tr>
-            })
+            for (1..=counter).map(|i| row(i) )
         }</table>
     }
     
     
 }
 
+fn row(row_index: i32) -> Html {
+    html! {
+        <tr>
+            <td>{ row_index }</td>
+            <td>{ "Hello World!" }</td>
+            <td>{ if row_index.is_even() {"even"} else {"odd"} }</td>
+        </tr>
+    }
+}
+
+fn basic_style() -> Html{
+    html! {
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+    }
+}
 
 fn main() {
     yew::start_app::<HelloWorld>();
