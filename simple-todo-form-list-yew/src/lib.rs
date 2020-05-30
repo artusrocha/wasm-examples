414814aa4b9664730e1cc6f096095ca7043954b0
Artus Rocha <artusrocha@gmail.com>
2020-05-30 01:23:14 +0000

    build with wasm-pack snippet

--- simple-todo-form-list-yew/src/lib.rs
+++ simple-todo-form-list-yew/src/lib.rs
@@ -13,7 +13,7 @@ use crate::todo::ToDo;
 pub fn run_app() {
     yew::initialize();
     let document = yew::utils::document();
-    let tags = document.get_elements_by_tag_name("Tag");
+    let tags = document.get_elements_by_tag_name("ToDo");
 //    App::<ToDo>::new().mount(tag.unwrap());
     for i in 0..tags.length() {
         App::<ToDo>::new().mount( tags.item(i).unwrap() ); //. .mount_to_body(); 
